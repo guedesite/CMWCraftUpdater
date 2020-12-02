4a049dd59d02b8dcae6ad47d3a9f61ec2c5f1a3a
Hugo MATHIEU <44479509+guedesite@users.noreply.github.com>
2020-12-02 12:19:56 +0100

    update

--- src/main.java
+++ src/main.java
@@ -19,10 +19,12 @@ public class main {
 	private static List<String> all = new ArrayList<String>();
 	
 	public static void main(String[] args) {
-		File baseDir = new File("base/");
+		long a = System.currentTimeMillis();
+		File baseDir = new File("oldVersion/");
 		try {
 			loopDir(baseDir);
-			System.out.println("end");
+			rmdir(new File("newVersion/"));
+			System.out.println("end in "+Math.round((System.currentTimeMillis() - a) /1000 ) + "s");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -43,7 +45,7 @@ public class main {
 			}
 			else {
 				File f3 = null;
-				if((f3 = new File(f.getAbsolutePath().replace("\\CMWCraftUpdater\\base\\", "\\CMWCraftUpdater\\change\\"))).exists()) {
+				if((f3 = new File(f.getAbsolutePath().replace("\\CMWCraftUpdater\\oldVersion\\", "\\CMWCraftUpdater\\newVersion\\"))).exists()) {
 					if(Arrays.equals(Files.readAllBytes(f.toPath()), Files.readAllBytes(f3.toPath()))) {
 						i++;
 						if(!f3.delete()) {
@@ -58,19 +60,61 @@ public class main {
 					}
 				}
 				else {
-					if(f3.createNewFile()) {
-						System.out.println("Create file "+f3.getAbsolutePath());
-					} else {
+					try  {
+						if(f3.createNewFile()) {
+							System.out.println("Create file "+f3.getAbsolutePath());
+						} else {
+							System.err.println("can't create file "+f3.getAbsolutePath());
+						}
+					} catch(Exception e) {
+						e.printStackTrace();
 						System.err.println("can't create file "+f3.getAbsolutePath());
 					}
 				}
 
 			}
 		}
-		if(nb == i && nb != 0) {
+	/*	if(nb == i && nb != 0) {
 			FileUtils.cleanDirectory(f2);
 			FileUtils.deleteDirectory(f2);
 			System.out.println("delete dir "+f2.getAbsolutePath());
-		}
+		} */
 	}
+	
+	public static boolean rmdir(final File folder) {
+	      if (folder.isDirectory()) {
+	          File[] list = folder.listFiles();
+	          int d = 0;
+	          if (list != null) {
+	              for (int i = 0; i < list.length; i++) {
+	                  File tmpF = list[i];
+	                  if (tmpF.isDirectory()) {
+	                      if(!rmdir(tmpF)) {
+	                    	  d = 9;
+	                      }
+	                  } else {
+	                	  d++;
+	                  }
+	              }
+	          }
+	          if(d == 0) {
+	        	  if (!folder.delete()) {
+	        		  try {
+						FileUtils.deleteDirectory(folder);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+	  	            System.out.println("can't delete folder : " + folder);
+	  	          } else {
+	  	        	  System.out.println("delete folder : " + folder);
+	  	          }
+	        	  return true;
+	          } else {
+	        	  return false;
+	          }
+
+	      }
+	      return true;
+	  }
 }
